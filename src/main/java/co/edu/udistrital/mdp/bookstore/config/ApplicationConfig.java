ad1825ab5505436de8b07f173eba374d8b57552f
Jose Bocanegra <josebocanegra@gmail.com>
2026-09-07 11:15:31 -0500

    Add initial files

--- src/main/java/co/edu/udistrital/mdp/bookstore/config/ApplicationConfig.java
+++ src/main/java/co/edu/udistrital/mdp/bookstore/config/ApplicationConfig.java
@@ -41,7 +41,8 @@ public class ApplicationConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
+                registry.addMapping("/**").allowedOrigins("http://localhost")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .maxAge(3600);
             }
 
